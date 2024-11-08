934adfa8061c301d915eb220c83bef1bf57c20cc
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-08 07:56:46 -0300

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dark mode",
+  description: "Aplicação de Dark Mode Toggle desenvolvido no curso de ReactJS da B7Web",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
